16ee9399cdb5d658c172d45506d066a543d05cf1
BENZEGHIOUA <jibril.benzeghioua@cgi.com>
2024-04-12 15:56:30 +0200

    Correction appel du service depuis le controller au lieu du repository

--- src/main/java/com/example/projet3chatop/controller/AuthController.java
+++ src/main/java/com/example/projet3chatop/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.example.projet3chatop.dto.UserDto;
 import com.example.projet3chatop.entity.User;
 import com.example.projet3chatop.exception.EmailAlreadyUsed;
 import com.example.projet3chatop.mapper.UserMapper;
+import com.example.projet3chatop.service.UserService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -27,18 +28,22 @@ public class AuthController {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final UserService userService;
 
     // Constructor injecting necessary dependencies.
     AuthController(AuthenticationManager authenticationManager,
                    PasswordEncoder passwordEncoder,
                    JwtUtils jwtUtils,
                    UserRepository userRepository,
+                   UserService userService,
                    UserMapper userMapper) {
         this.authenticationManager = authenticationManager;
         this.jwtUtils = jwtUtils;
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.userService = userService;
+
     }
 
     // Endpoint for user authentication.
@@ -53,33 +58,21 @@ public class AuthController {
         return map;
     }
 
-    // Endpoint for registering a new user.
     @PostMapping("/register")
     public Map<String, String> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-            throw new EmailAlreadyUsed();
-        }
+        User registeredUser = userService.registerUser(signUpRequest);
 
-        // Creating a new user with encrypted password.
-        User user = new User(
-                signUpRequest.getEmail(),
-                false,
-                signUpRequest.getName(),
-                passwordEncoder.encode(signUpRequest.getPassword())
-        );
-        userRepository.save(user);
-
-        Map<String,String> map=new HashMap<>();
-        map.put("token", jwtUtils.generateJwtToken(signUpRequest.getEmail()));
+        Map<String, String> response = new HashMap<>();
+        response.put("token", jwtUtils.generateJwtToken(registeredUser.getEmail()));
 
         // Response indicating successful user registration.
-        return  map;
+        return response;
     }
 
     // Endpoint to retrieve profile details of the currently logged-in user.
     @GetMapping("/me")
     public UserDto userProfile(@RequestHeader(value="Authorization",required=false) String jwt) {
         // Retrieving username from JWT token and converting to DTO.
-        return userMapper.toDto(userRepository.findByEmail(jwtUtils.getUserNameFromJwtToken(jwt.substring(7))).get());
+        return userMapper.toDto(userService.findbyEmail(jwtUtils.getUserNameFromJwtToken(jwt.substring(7))));
     }
 }
--- src/main/java/com/example/projet3chatop/service/UserService.java
+++ src/main/java/com/example/projet3chatop/service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.projet3chatop.service;
 
 import com.example.projet3chatop.entity.User;
+import com.example.projet3chatop.exception.EmailAlreadyUsed;
+import com.example.projet3chatop.payload.request.SignupRequest;
 import com.example.projet3chatop.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,10 +13,14 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
 
     // Constructor taking UserRepository repository as parameter
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     // Method to retrieve a user by their email
@@ -29,5 +36,25 @@ public class UserService {
     // Method to find a user by their ID
     public User findById(Long id) {
         return this.userRepository.findById(id).orElse(null);
+    }
+
+    public User findbyEmail(String email) {
+        return this.userRepository.findByEmail(email).orElse(null);
+    }
+
+    // Method to register a new user
+    public User registerUser(SignupRequest signUpRequest) {
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new EmailAlreadyUsed();
+        }
+
+        // Creating a new user with encrypted password.
+        User user = new User(
+                signUpRequest.getEmail(),
+                false,
+                signUpRequest.getName(),
+                passwordEncoder.encode(signUpRequest.getPassword())
+        );
+        return userRepository.save(user);
     }
 }
